555fd5efa75bcf933c552ca1b5706bcfae750670
Shaga3app <144626891+Shaga3app@users.noreply.github.com>
2026-01-13 16:38:13 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "shaga3app-pwa-v1";
+
+// Keep this minimal so you do not accidentally cache dynamic HTML during testing.
+const ASSETS = [
+  "./",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((hit) => hit || fetch(event.request))
+  );
+});
